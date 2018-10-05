3e3426b32f430ee397f271fb1788de19d6b09049
Erickson Teixeira <erickson.teixeira@linkka.com.br>
2018-10-05 02:04:51 -0300

    Autenticando user com secao e destruindo secao

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,26 @@
 module.exports.index = function(application, req, res){
-	res.render('index');
+	res.render('index',{validacao : {}});
+	//json vazio da var validacao,para que o ejs entenda a pag index
+}
+
+module.exports.autenticar = function(application, req, res){
+	
+	var dadosForm = req.body;
+
+	req.assert('usuario', 'Usuário não pode estar vazio!!').notEmpty();
+	req.assert('senha', 'Senha não pode estar vazia!!').notEmpty();
+
+	var erros = req.validationErrors();
+
+	if (erros) {
+		res.render('index', {validacao : erros});
+		return;
+	}
+
+	var connection = application.config.dbConnection;
+	var UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+
+	UsuariosDAO.autenticar(dadosForm, req, res);
+
+	//res.send('tudo ok para criar sessão');
 }
--- app/models/UsuariosDAO.js
+++ app/models/UsuariosDAO.js
@@ -3,11 +3,34 @@ function UsuariosDAO(connection){
 }
 
 UsuariosDAO.prototype.inserirUsuario = function(usuario){
-						//open param callback
 	this._connection.open(function(err, mongoclient){
-					//collection param collection name,callback
 		mongoclient.collection("usuarios", function(err, collection){
 			collection.insert(usuario);
+			mongoclient.close();
+		});
+	});
+}
+
+UsuariosDAO.prototype.autenticar = function(usuario, req, res){
+	this._connection.open(function(err, mongoclient){
+		mongoclient.collection("usuarios", function(err, collection){
+			collection.find(usuario).toArray(function(err, result){
+				//se o array usuario não estiver vazio,iremos gerar uma session "autorizado"
+				if (result[0] != undefined) {
+
+					req.session.autorizado = true;
+					req.session.usuario = result[0].usuario;
+					req.session.casa = result[0].casa;
+				}
+
+				if (req.session.autorizado) {
+					res.redirect("jogo");
+				}else {
+					res.render("index", {validacao : {}}); 
+					// passando a var vazia,para que não de erro. Lembrando que ela é parametro do render index
+				}
+			});
+
 			mongoclient.close();
 		});
 	});
